6758fff860e0486b70648059665a3898df1bb079
Wesleyss071299 <wesleysilvadesousa@gmail.com>
2023-11-30 07:49:50 -0300

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex max-h-screen">
-      <div className="flex flex-col	 bg-primary min-h-screen min-w-[34%] pl-80px pt-80px pb-80px rounded-r-3xl justify-between relative">
+    <div className="flex max-h-screen">
+      <div className="hidden md:flex  flex-col justify-between h-screen bg-primary md:overflow-hidden overflow-auto md:hover:overflow-auto px-6 py-6">
         <Image src={"/logo.svg"} alt="logo" width={62} height={62} />
         <div className="flex flex-col">
           <span className="text-white text-6xl font-semibold max-w-[400px]">
@@ -18,22 +18,22 @@ export default function Home() {
           Fale Conosco
         </button>
       </div>
-      <div className="flex flex-col bg-white min-h-screen flex-1 pl-[100px] py-[70px] overflow-y-scroll">
+      <div className="flex flex-col bg-white min-h-screen flex-1 px-6 py-[70px] overflow-y-scroll">
         <span className="text-primary text-[73px] font-semibold">
           Faça Login
         </span>
         <span className="text-primary text-[20px] font-semibold mt-[125px]">
           Seu CPF
         </span>
-        <input className="w-[80%] min-h-[73px] max-w-[90%] border-primary  border-2 rounded-2xl focus:ring-secondary focus:ring-inset focus:ring-2 focus:shadow-secondary focus:border-secondary focus:outline-none" />
+        <input className=" min-h-[73px] md:max-w-[90%] border-primary  border-2 rounded-2xl focus:ring-secondary focus:ring-inset focus:ring-2 focus:shadow-secondary focus:border-secondary focus:outline-none" />
         <span className="text-primary text-[20px] font-semibold mt-[60px]">
           Senha
         </span>
-        <input className="w-[80%] min-h-[73px] max-w-[90%] border-primary border-2 rounded-2xl focus:ring-secondary focus:ring-inset focus:ring-2 focus:shadow-secondary focus:border-secondary focus:outline-none" />
+        <input className="min-h-[73px] md:max-w-[90%] border-primary border-2 rounded-2xl focus:ring-secondary focus:ring-inset focus:ring-2 focus:shadow-secondary focus:border-secondary focus:outline-none" />
         <span className="cursor-pointer text-secondary text-[13px] font-medium underline decoration-2">
           Esqueci minha sennha
         </span>
-        <Link  href={'/dashboard'} className="max-w-[197px] pt-[15px] pb-[15px] pl-[40px] pr-[40px] bg-secondary text-[15px] font-semibold rounded-xl hover:scale-105 mt-[120px]">
+        <Link  href={'/dashboard'} className="w-[197px] pt-[15px] pb-[15px] pl-[40px] pr-[40px] bg-secondary text-[15px] font-semibold rounded-xl hover:scale-105 mt-[120px]">
           Faça seu Login
         </Link>
         <div className="flex flex-col justify-center items-center">
@@ -42,6 +42,6 @@ export default function Home() {
           </button>
         </div>
       </div>
-    </main>
+    </div>
   );
 }
