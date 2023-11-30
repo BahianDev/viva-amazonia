f93288f22a06564db88799a9f33a9bc7121850f4
Wesleyss071299 <wesleysilvadesousa@gmail.com>
2023-11-30 08:22:56 -0300

    fix: responsive

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -1,15 +1,24 @@
 import React from "react";
 import Image from "next/image";
 import { useFormState } from "../contexts/FormContext";
+import { IoIosClose } from "react-icons/io";
 
 const Sidebar = () => {
-  const { activeMenu } =
-    useFormState();
+  const { activeMenu, setActiveMenu } = useFormState();
   return (
     <div className="h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto px-6 py-6">
       {activeMenu && (
         <>
-          <Image src={"/logo_dark.svg"} alt="logo" width={62} height={62} />
+          <div className="flex justify-between">
+            <Image src={"/logo_dark.svg"} alt="logo" width={62} height={62} />
+            <button
+              type="button"
+              onClick={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)}
+              className="text-5xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
+            >
+              <IoIosClose />
+            </button>
+          </div>
           <div className="flex flex-col gap-[50px] mt-[100px]">
             <div className="flex items-center cursor-pointer">
               <Image
@@ -55,7 +64,6 @@ const Sidebar = () => {
                 Configurações
               </span>
             </div>
-            
           </div>
           <button className="max-w-[197px] pt-[15px] pb-[15px] pl-[40px] pr-[40px] bg-secondary text-[15px] font-semibold rounded-xl hover:scale-105 mt-[120px]">
             Fale Conosco
