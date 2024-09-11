e959210888dddec8b857e881b12f916baea8ad9e
Wesleyss071299 <wesleysilvadesousa@gmail.com>
2024-09-11 12:58:20 -0300

    fix: url

--- src/lib/configs/auth/authOptions.ts
+++ src/lib/configs/auth/authOptions.ts
@@ -7,7 +7,7 @@ import CredentialsProvider from "next-auth/providers/credentials";
 
 async function refreshToken(token: any): Promise<JWT> {
     console.log("refresh");
-    const res = await fetch("https://personal-beta-b666c12c6edc.herokuapp.com/auth/refresh", {
+    const res = await fetch("https://viva-amazonia-b7d621e98f23.herokuapp.com/auth/refresh", {
       method: "POST",
       headers: {
         authorization: `Bearer ${token.refreshToken}`,
@@ -41,7 +41,7 @@ export const authOptions: NextAuthOptions = {
         async authorize(credentials, req) {
           if (!credentials?.username || !credentials?.password) return null;
           const { username, password } = credentials;
-          const res = await fetch("https://personal-beta-b666c12c6edc.herokuapp.com/auth/login", {
+          const res = await fetch("https://viva-amazonia-b7d621e98f23.herokuapp.com/auth/login", {
             method: "POST",
             body: JSON.stringify({
               username,
