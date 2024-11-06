827322ff73757de8312d01201de07f32d41588ff
Wesleyss071299 <wesleysilvadesousa@gmail.com>
2024-11-06 17:58:00 -0300

    fix: home

--- src/app/dashboard/home/page.tsx
+++ src/app/dashboard/home/page.tsx
@@ -6,7 +6,6 @@ import { useQuery } from "@tanstack/react-query";
 import { api } from "@/services/api";
 import TipoAreaChart from "@/components/TipoAreaChart";
 
-
 const HomePage = () => {
   const { data: info } = useQuery({
     queryKey: ["files-list"],
@@ -23,7 +22,7 @@ const HomePage = () => {
 
   return (
     <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
-      <div className="flex flex-col md:flex-row max-w-7xl justify-between items-center p-5 rounded-2xl border-2 gap-5 bg-white  mt-6">
+      {/* <div className="flex flex-col md:flex-row max-w-7xl justify-between items-center p-5 rounded-2xl border-2 gap-5 bg-white  mt-6">
         <Image
           src={"/circle.svg"}
           width={51}
@@ -41,37 +40,7 @@ const HomePage = () => {
         >
           Continue seu Cadastro
         </Link>
-      </div>
-
-      <div className="flex flex-col mt-8">
-        <div className="flex gap-2 items-center">
-          <Image
-            src={"/benefits.svg"}
-            alt="benefits"
-            width={24}
-            height={24}
-            className="w-6 h-6"
-          />
-          <span className="text-lg font-medium">Seus Benefícios</span>
-        </div>
-        <div className="flex flex-wrap lg:flex-nowrap mt-3 gap-8">
-          {beneficiosData.map((item, index) => (
-            <div
-              key={item.id}
-              className="flex flex-col w-96 bg-white rounded-2xl justify-between border-2 p-5 gap-1"
-            >
-              <div className="w-24 h-24 bg-midGray rounded-2xl p-5">
-                <div className="w-full h-full rounded-full border-2 border-white" />
-              </div>
-              <span className="text-gray-500 text-lg">{item.title}</span>
-              <p className="text-gray-400 text-sm">{item.description}</p>
-              <button className="p-2 bg-midGray text-sm text-gray-200 font-semibold rounded-xl hover:scale-105 mt-4">
-                Conclua Seu Cadastro
-              </button>
-            </div>
-          ))}
-        </div>
-      </div>
+      </div> */}
 
       <div className="flex mt-8">
         <div className="flex flex-col md:flex-row gap-8">
@@ -115,7 +84,7 @@ const HomePage = () => {
                 Regularizações
               </span>
             </div>
-            <div className="bg-white gap-3 flex flex-col p-5 rounded-2xl mt-3 border-2">
+            <div className="bg-white gap-3 justify-between flex flex-col p-5 rounded-2xl mt-3 border-2 min-h-[489px]">
               <Image
                 src={"/graph.svg"}
                 width={144}
@@ -170,6 +139,35 @@ const HomePage = () => {
               </button>
             </div>
           </div>
+        </div>
+      </div>
+      <div className="flex flex-col mt-8">
+        <div className="flex gap-2 items-center">
+          <Image
+            src={"/benefits.svg"}
+            alt="benefits"
+            width={24}
+            height={24}
+            className="w-6 h-6"
+          />
+          <span className="text-lg font-medium">Seus Benefícios</span>
+        </div>
+        <div className="flex flex-wrap lg:flex-nowrap mt-3 gap-8">
+          {beneficiosData.map((item, index) => (
+            <div
+              key={item.id}
+              className="flex flex-col w-96 bg-white rounded-2xl justify-between border-2 p-5 gap-1"
+            >
+              <div className="w-24 h-24 bg-midGray rounded-2xl p-5">
+                <div className="w-full h-full rounded-full border-2 border-white" />
+              </div>
+              <span className="text-gray-500 text-lg">{item.title}</span>
+              <p className="text-gray-400 text-sm">{item.description}</p>
+              <button className="p-2 bg-midGray text-sm text-gray-200 font-semibold rounded-xl hover:scale-105 mt-4">
+                Conclua Seu Cadastro
+              </button>
+            </div>
+          ))}
         </div>
       </div>
     </div>
